692a4918dab57fb846e445ebfed15ed1e7d60a6e
Angel Petrov <angel.petrov@limechain.tech>
2023-10-17 13:23:13 +0300

    Make bytes_to_ip_addr use a reference

--- network/src/tls/mod.rs
+++ network/src/tls/mod.rs
@@ -3,7 +3,7 @@ use std::net::IpAddr;
 pub mod certificate;
 pub mod client;
 
-pub fn bytes_to_ip_addr(bytes: Vec<u8>) -> Option<IpAddr> {
+pub fn bytes_to_ip_addr(bytes: &[u8]) -> Option<IpAddr> {
     let bytes: [u8; 16] = bytes.try_into().ok()?;
 
     let ip_addr = IpAddr::from(bytes);
